53c1f58171fc771e7bf59692d166cd6a954632ef
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-08-25 13:41:52 +1000

    updates

--- highscore.js
+++ highscore.js
@@ -1,6 +1,13 @@
 var userResults = document.querySelector("#userResults");
+var nameEl = document.querySelector("#nameEl");
+var scoreEL = document.querySelector("#scoreEl");
 
 var username = JSON.parse(localStorage.getItem("username"));
 var userscore = localStorage.getItem("score");
+console.log(username);
+console.log(userscore);
 
 userResults.textContent = ("User Name" + username, "Score " + userscore);
+
+nameEl.textContent = username;
+scoreEL.textContent = userscore;
